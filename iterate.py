671cc768fe531abe91bb3787f9f3a01d2f517904
JohnDunthorne <johndunthorne1@gmail.com>
2023-10-01 11:10:33 -0500

    1

---
+++ iterate.py
@@ -0,0 +1,12 @@
+numbers = [1, 2, 3, 4, 5, 7, 9]
+
+for each_item in numbers:
+    print(each_item + 1)
+
+letters = ["a", "g", "y", "e", "y", "s", "y", "s"]
+
+for each_item in letters:
+    print(each_item * 2)
+
+for item in range(len(letters)):
+    print(item, letters[item])
---
+++ woof.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int woof;
+    int input;
+    for (woof = 0; woof < 5; woof++)
+        ;
+    printf("woof\n");
+}
